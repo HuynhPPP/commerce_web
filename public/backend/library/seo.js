761e4b45903d08ce149df3db0666376ddd90a62a
HuynhPPP <tankhuong02@gmail.com>
2024-07-01 21:17:53 +0700

    update_func_removeUTF8

--- public/backend/library/seo.js
+++ public/backend/library/seo.js
@@ -15,7 +15,7 @@
 
         $('input[name=canonical]').on('keyup', function(){
             let input = $(this)
-            let value = input.val()
+            let value = HT.removeUtf8(input.val())
             $('.canonical').html(BASE_URL + value + SUFFIX) 
         })
 
@@ -25,6 +25,22 @@
             $('.meta_description').html(value)
         })
     }
+
+    HT.removeUtf8 = (str) => {
+        str = str.toLowerCase(); // Chuyển về kí tự thường
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ã|ă|ằ|ắ|ặ|ả|å/g, "a");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ồ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+        str = str.replace(/đ/g, "d");
+        str = str.replace(/[!@%^*()+=<>?1,]/g, ""); // Thay thế các ký tự đặc biệt
+        str = str.replace(/-+/g, "-"); // Loại bỏ dấu gạch ngang liên tiếp
+        str = str.replace(/\s+/g, "-"); // Thay thế khoảng trắng bằng dấu gạch ngang
+        str = str.replace(/^-+|-+$/g, ""); // Loại bỏ dấu gạch ngang ở đầu và cuối chuỗi
+        return str;
+    };
         
     $(document).ready(function(){
         HT.seoPreview();
